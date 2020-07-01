d06543ddb7cc8e52fedb8839e02a27512465a6f6
Engevix <p.kobylyatskiy@gmail.com>
2020-07-01 18:38:00 +0300

    Everything until the moment specified in the description

---
+++ README.md
@@ -0,0 +1,4 @@
+https://youtu.be/V1rhxheJg4A
+00:59:10 код написан до этого момента
+
+``yarn install`` ,``yarn start``
---
+++ src/components/Form.js
@@ -0,0 +1,36 @@
+import React, {useState, useContext} from "react";
+import {AlertContext} from "../context/alert/alertContext";
+
+export const Form = () => {
+
+  const [value, setValue] = useState('')
+
+  const alert = useContext(AlertContext)
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    if (value.trim()) {
+      // ..
+      alert.show('Note was create', 'success')
+      setValue('')
+    } else {
+      alert.show('Enter note text')
+    }
+
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-group">
+        <input
+          type='text'
+          className="form-control"
+          placeholder='Enter note'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </form>
+  )
+}
